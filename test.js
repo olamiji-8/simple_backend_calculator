4b0f4d682ef5d61e508f5144d6906b452222708c
olamiji-8 <olamijisotomi@gmail.com>
2022-09-28 11:56:24 +0100

    update

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
+  const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+app.put(bodyParser.urlencoded({extended:true}));
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html");
 })
+var num1 =Number(req.body.num1);
+var num2 = Number(req.body.num2);
+var result = num1 + num2 ;
+
 app.post("/", function(req,res){
-    res.send("Thank you for responding.");
+    res.send("The result of the calculator is " + result );
 })
 
 app.listen(3000,function(){

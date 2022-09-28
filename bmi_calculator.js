c8373adc4c8020347cc1f1a94e90a39b6619d8f1
olamiji-8 <olamijisotomi@gmail.com>
2022-09-28 14:58:00 +0100

    update

---
+++ bmi_calculator.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { SelectionDirection } = require("monaco-editor");
+
+const app = express();
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/bmi_calculator.js");
+});
+
+app.post("/", function(){
+    res.send("You have been accepted");
+});
+
+app.listen(3000 , function(){
+    console.log("You are on port 3000");
+});
+    
+
